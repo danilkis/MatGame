f6476b7b44806e221b2e44fdde0806da0aa1653f
danilkis <danyapancresnicov@gmail.com>
2022-12-13 21:30:59 +0300

    Вывод седловой точки в консоль

--- MatGame/Program.cs
+++ MatGame/Program.cs
@@ -3,24 +3,21 @@ var M = Convert.ToInt16(Console.ReadLine());
 if (M > 10 && M < 2)
 {
     Console.WriteLine("Вне диапазона");
-    return 0;
 }
 Console.WriteLine("Введите размер матрицы N (от 2 до 10)");
 var N = Convert.ToInt16(Console.ReadLine());
 if (N > 10 && N < 2)
 {
     Console.WriteLine("Вне диапазона");
-    return 0;
 }
 int i, j;
 int[,] matrix = new int[N, M];
 
 List<int> row = new List<int>();
-List<int> col = new List<int>();
 List<int> price1 = new List<int>();
 List<int> price2 = new List<int>();
-List<Tuple<int, int>> price_pos= new List<Tuple<int,int>>();
-List<Tuple<int, int>> row_pos= new List<Tuple<int,int>>();
+List<Tuple<int, int>> temp_pos = new List<Tuple<int, int>>();
+List<Tuple<int, int>> price_pos = new List<Tuple<int, int>>();
 for (int o = 0; o < M; o++)
 {
     for (int p = 0; p < N; p++)
@@ -28,16 +25,15 @@ for (int o = 0; o < M; o++)
         Random rnd = new Random();
         int rand_num = rnd.Next(2, 12);
         matrix[p, o] = rand_num;
-        Console.Write(matrix[p, o].ToString() + "\t");
-        row_pos.Add(Console.GetCursorPosition().ToTuple());
+        Console.Write(matrix[p, o] + "\t");
         row.Add(matrix[p, o]);
+        temp_pos.Add(Tuple.Create(p, o));
         if (p == N - 1)
         {
             price1.Add(row.Min());
-            price_pos.Add(row_pos[row.IndexOf(Convert.ToInt16(row.Min()))]);
             Console.Write("\t" + "|" + row.Min());
-            Console.GetCursorPosition();
-            row_pos.Clear();
+            price_pos.Add(temp_pos[row.IndexOf(row.Min())]);
+            temp_pos.Clear();
             row.Clear();
         }
     }
@@ -53,6 +49,7 @@ for (int o = 0; o < M; o++)
             for (int z = 0; z < M; z++)
             {
                 column.Add(matrix[y,z]);
+                price_pos.Add(Tuple.Create(y,z));
             }
             Console.Write(column.Max() + "\t");
             price2.Add(column.Max());
@@ -60,24 +57,22 @@ for (int o = 0; o < M; o++)
         }
     }
 }
-
-Console.WriteLine("\nМинимальное значение в строках: ");
-Console.WriteLine("Нижняя цена: "+ price1.Max());
+Console.WriteLine(" ");
 Console.WriteLine("Верхняя цена: "+ price2.Min());
-/*
-foreach (var tup in price_pos)
-{
-    Console.BackgroundColor= ConsoleColor.Red;
-    Console.SetCursorPosition(tup.Item1, tup.Item2);
-    Console.Write('*');
-}
-*/
+Console.WriteLine("Нижняя цена: "+ price1.Max());
 if (price1.Max() == price2.Min())
 {
     Console.WriteLine("Чистые стратегии есть");
-    
-} else
+    Tuple<int, int> temp_tup = new Tuple<int, int>(0,0);
+    foreach (Tuple<int,int> tup in price_pos)
+    {
+        if (tup.Item1 == temp_tup.Item1 && tup.Item2 == temp_tup.Item2)
+        {
+            Console.WriteLine("Седловая точка на координатах: " + tup.Item1 + ";" + tup.Item2 );
+        }
+    }
+} 
+else
 {
     Console.WriteLine("Решения игры в чистых стратегиях не существует!");
 }
-return 0;
